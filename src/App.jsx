83561e5f29c0e422a815e7ca93d761fd819db9da
MiaPham2520 <mia2520@uw.edu>
2024-04-08 15:35:01 -0700

    working on the about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Footer from "./components/footer";
 import HomePage from "./components/homepage";
+import AboutUs from "./components/aboutus";
 import Navbar from "./components/navbar";
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route index element={<HomePage />} />
+        <Route path="/aboutus" element={<AboutUs />} />
       </Routes>
       <Footer />
     </>
--- src/components/aboutus.jsx
+++ src/components/aboutus.jsx
@@ -1,5 +1,20 @@
+import "../css/about.css";
+
 const AboutUs = () => {
-  return <></>;
+  return (
+    <main className="about_us">
+      <h1>About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+        convallis nunc eget lectus porttitor, quis varius magna convallis.
+        Quisque ut augue nulla. Nunc porttitor ac dolor nec ultrices. Phasellus
+        scelerisque velit eu velit molestie accumsan. Vivamus pretium massa
+        urna. Curabitur eget quam viverra, gravida risus imperdiet, sollicitudin
+        nisi. Duis aliquam efficitur nulla in scelerisque. Cras laoreet ipsum
+        eros, sit amet convallis nisl egestas sit amet.
+      </p>
+    </main>
+  );
 };
 
 export default AboutUs;
