e4dd3cad16a30854ae301a43a84da4b1a7e24188
Gabriella Rivera <gabriella7r@gmail.com>
2024-04-09 19:51:24 -0700

    about us page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Footer from "./components/footer";
 import HomePage from "./components/homepage";
 import Navbar from "./components/navbar";
+import AboutUs from "./components/aboutus";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route index element={<HomePage />} />
+        <Route path="aboutus" element={<AboutUs />} />
       </Routes>
       <Footer />
     </>
