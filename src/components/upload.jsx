0aaafc28e160c1ea0e7fca289bda40bd06cbda05
MiaPham2520 <mia2520@uw.edu>
2024-04-12 17:48:13 -0700

    Upper part of upload form

--- src/components/upload.jsx
+++ src/components/upload.jsx
@@ -1,10 +1,126 @@
+import "../css/upload.css";
+
 const Upload = () => {
   return (
     <main>
       <h1>Upload Syllabus</h1>
+      <form className="upload_form" aria-label="Course syllabus upload form">
+        <label htmlFor="Professor First and Last Name">
+          Professor&apos;s First and Last Name*
+          <span>
+            <input
+              type="text"
+              id="professor"
+              placeholder="First and Last Name"
+              required
+            />
+          </span>
+        </label>
+        <label htmlFor="Course Name">
+          Course Name*
+          <span>
+            <input
+              type="text"
+              id="course_name"
+              placeholder="E.g. Client-Side Web Development"
+              required
+            />
+          </span>
+        </label>
+        <label htmlFor="Course Code">
+          Course Code*
+          <span>
+            <input
+              type="text"
+              id="course_code"
+              placeholder="E.g. INFO 340"
+              required
+            />
+          </span>
+        </label>
+        <label htmlFor="Year Offered">
+          Year Offered*
+          <span>
+            <input
+              type="text"
+              id="year_offered"
+              placeholder="E.g. 2024"
+              required
+            />
+          </span>
+        </label>
+        <label htmlFor="Quarter Offered">
+          Quarter Offered*
+          <ul className="items-center w-full rounded-lg sm:flex">
+            <li>
+              <div className="flex items-center ps-3">
+                <input
+                  id="autumn-checkbox-list"
+                  type="checkbox"
+                  value=""
+                  className="w-8 h-8 rounded focus:ring-2"
+                />
+                <label
+                  htmlFor="autumn-checkbox-list"
+                  className="w-full py-3 ms-2 text-xl"
+                >
+                  Autumn
+                </label>
+              </div>
+            </li>
+            <li className="w-full">
+              <div className="flex items-center ps-3">
+                <input
+                  id="winter-checkbox-list"
+                  type="checkbox"
+                  value=""
+                  className="w-8 h-8 rounded focus:ring-2"
+                />
+                <label
+                  htmlFor="winter-checkbox-list"
+                  className="w-full py-3 ms-2 text-xl"
+                >
+                  Winter
+                </label>
+              </div>
+            </li>
+            <li className="items-center	">
+              <div className="flex items-center ps-3">
+                <input
+                  id="spring-checkbox-list"
+                  type="checkbox"
+                  value=""
+                  className="w-8 h-8 rounded focus:ring-2"
+                />
+                <label
+                  htmlFor="spring-checkbox-list"
+                  className="w-full py-3 ms-2 text-xl"
+                >
+                  Spring
+                </label>
+              </div>
+            </li>
+            <li className="w-full dark:border-gray-600">
+              <div className="flex items-center ps-3">
+                <input
+                  id="summer-checkbox-list"
+                  type="checkbox"
+                  value=""
+                  className="w-8 h-8 rounded focus:ring-2"
+                />
+                <label
+                  htmlFor="summer-checkbox-list"
+                  className="w-full py-3 ms-2 text-xl"
+                >
+                  Summer
+                </label>
+              </div>
+            </li>
+          </ul>
+        </label>
+      </form>
     </main>
   );
 };
-
 
 export default Upload;
